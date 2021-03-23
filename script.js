6053496a6f4ac77a05f423ef1292de3df9c1d68e
geniuskingsley <53303797+geniuskingsley@users.noreply.github.com>
2021-03-23 21:46:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-/**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
- * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
- */
 
-
-/**
- * Update the output text as a user types in the textarea
- * HINT: Use the onkeydown function inside HTML
- */
 function updateText() {
 
   //we are collecting the value from the text area... by using on key down
@@ -19,13 +8,7 @@ function updateText() {
   document.getElementById('text-output').innerText = text;
 }
 
-/**
- * Toggle the bold class for the output text
- * HINT: Use the onclick function insite HTML
- * HINT: Look into using this keyword
- * HINT: Use the classList property
- * HINT: Toggle .active class for the button
- */
+
 function makeBold(elem) {
   
   //we are making our button active when we click on it..
@@ -44,12 +27,7 @@ function makeItalic(elem) {
   document.getElementById('text-output').classList.toggle('italic');
 }
 
-/**
- * Toggle the underline class for the output text
- * HINT: Toggle the .active class for the button
- * HINT: Use the classList property
- * HINT: Use contains, remove, and add functions
- */
+
 function makeUnderline(elem){
   elem.classList.toggle('active');
 
@@ -65,12 +43,7 @@ function makeUnderline(elem){
   }
 }
 
-/**
- * Toggle the style textAlign attribute
- * Toggle the active state for the align butttons
- * HINT: Use the style property of the element
- * HINT: Make sure to untoggle the active state for all other align buttons
- */
+
 function alignText(elem, alignType){
 
   //we are aligning text base on the id if its a left, etc..
